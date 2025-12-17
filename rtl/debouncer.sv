1d71cfb99028198534a14edc9355323eeb586377
Ciro Bermudez <cirofabian.bermudezmarquez@ba.infn.it>
2025-12-17 12:19:20 +0100

    feat: add versions of RTL to verify with assertions

--- rtl/debouncer.sv
+++ rtl/debouncer.sv
@@ -81,7 +81,115 @@ module debouncer #(
 
   assign db_level_o = ff3;
 
-  // Output single tick with edge detector
+`ifdef ERROR1_VERSION
+
+  // Output single tick with edge detector (ERROR)
+  always_ff @(posedge clk_i, posedge rst_i) begin
+    if (rst_i) begin
+      ff4 <= 'd0;
+    end else if (ena_cnt) begin
+      ff4 <= ~ff3 & ff2;
+    end
+    // else begin // This is the fix that makes it correct
+    //   ff4 <= 1'b0;
+    // end
+  end
+
+  assign db_tick_o = ff4;
+
+`elsif ERROR2_VERSION
+
+  // Output single tick (CORRECT/SEQUENTIAL OUTPUT)
+  typedef enum {ST_IDLE, ST_ACTIVE} state_type_e;
+  state_type_e state_reg, state_next;
+  logic ff4_next;
+
+  // FSMD State and Data Registers
+  always_ff @(posedge clk_i, posedge rst_i) begin
+    if (rst_i) begin
+      state_reg <= 'd0;
+      ff4       <= 'd0;
+    end else begin
+      state_reg <= state_next;
+      ff4       <= ff4_next;
+    end
+  end
+  
+  // FSMD Next State Logic and Output Logic
+  always_comb begin
+    state_next = state_reg;
+    ff4_next   = ff4;
+    case (state_reg)
+      ST_IDLE: begin
+        if (ena_cnt & ff2) begin
+          ff4_next = 1'b1;
+          state_next = ST_ACTIVE;
+        end
+      end
+      ST_ACTIVE: begin
+        ff4_next = 1'b0;
+        state_next = ST_IDLE;
+      end
+    endcase
+  end
+  assign db_tick_o = ff4;
+
+`elsif SEQ1_VERSION
+
+  // Best solution for sequential output
+  always_ff @(posedge clk_i or posedge rst_i) begin
+    if (rst_i) begin
+      ff4 <= 1'b0;
+    end else begin
+      ff4 <= ena_cnt & ff2 & ~ff3; // old ff3, current stable sample ff2
+    end
+  end
+  assign db_tick_o = ff4;
+
+`elsif SEQ2_VERSION
+
+  // Using the FSM is overkill but it works
+
+  // Output single tick (CORRECT/SEQUENTIAL OUTPUT)
+  typedef enum {ST_IDLE, ST_ACTIVE} state_type_e;
+  state_type_e state_reg, state_next;
+  logic ff4_next;
+
+  // FSMD State and Data Registers
+  always_ff @(posedge clk_i, posedge rst_i) begin
+    if (rst_i) begin
+      state_reg <= 'd0;
+      ff4       <= 'd0;
+    end else begin
+      state_reg <= state_next;
+      ff4       <= ff4_next;
+    end
+  end
+  
+  // FSMD Next State Logic and Output Logic
+  always_comb begin
+    state_next = state_reg;
+    ff4_next   = ff4;
+    case (state_reg)
+      ST_IDLE: begin
+        if (ena_cnt & ff2 & ~ff3) begin
+          ff4_next = 1'b1;
+          state_next = ST_ACTIVE;
+        end
+      end
+      ST_ACTIVE: begin
+        ff4_next = 1'b0;
+        if (ena_cnt & ~ff2 & ff3) begin
+          state_next = ST_IDLE;
+        end
+      end
+    endcase
+  end
+  assign db_tick_o = ff4;
+
+`else
+
+  // Output single tick (CORRECT/COMBINATIONAL OUTPUT)
   always_ff @(posedge clk_i, posedge rst_i) begin
     if (rst_i) begin
       ff4 <= 'd0;
@@ -89,7 +197,8 @@ module debouncer #(
       ff4 <= ff3;
     end
   end
+  assign db_tick_o = ~ff4 & ff3;
 
-  assign db_tick_o = ff4;
+`endif
 
 endmodule
