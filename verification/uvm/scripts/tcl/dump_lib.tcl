53ba5d0ec74985e5b17822446b62c6e0126f1b31
Ciro Bermudez <cirofabian.bermudezmarquez@ba.infn.it>
2026-05-07 17:10:07 +0200

    feat: add easier dumping variables and tcl scripts

--- verification/uvm/scripts/tcl/dump_lib.tcl
+++ verification/uvm/scripts/tcl/dump_lib.tcl
@@ -1,4 +1,5 @@
-## [Filename]       dump.tcl
+##=============================================================================
+## [Filename]       dump_lib.tcl
 ## [Project]        -
 ## [Author]         Ciro Bermudez - cirofabian.bermudezmarquez@ba.infn.it
 ## [Language]       Tcl (Tool Command Language)
@@ -15,9 +16,9 @@
 ## [Revisions]      -
 ##=============================================================================
 
-proc dump_signals {} {
+proc dump_default {} {
     # Dump signals to FSDB (RECOMMENDED)
-    puts "\[TCL-CUSTOM\]: Dumping signals to novas.fsdb"
+    puts {[TCL-CUSTOM]: Dumping dut signals to novas.fsdb}
     dump -file novas.fsdb -type FSDB
     dump -add tb.dut -depth 0 -fid FSDB0
     run
@@ -27,7 +28,7 @@ proc dump_signals {} {
 
 proc dump_all {} {
     # Dumps everything to FSDB from root including complex data type
-    puts "\[TCL-CUSTOM\]: Dumping all signals to novas.fsdb"
+    puts {[TCL-CUSTOM]: Dumping all signals to novas.fsdb}
     dump -file novas.fsdb -type FSDB
     dump -add / -aggregates -fid FSDB0
     run
@@ -37,7 +38,7 @@ proc dump_all {} {
 
 proc dump_top {} {
     # Dump signals to FSDB from dut just ports
-    puts "\[TCL-CUSTOM\]: Dumping port signals to novas.fsdb"
+    puts {[TCL-CUSTOM]: Dumping port signals to novas.fsdb}
     dump -file novas.fsdb -type FSDB
     dump -add tb.dut -depth 1 -ports -fid FSDB0
     run
@@ -47,7 +48,7 @@ proc dump_top {} {
 
 proc dump_custom {} {
     # Dump signals to FSDB specific signals
-    puts "\[TCL-CUSTOM\]: Dumping specific signals to novas.fsdb"
+    puts {[TCL-CUSTOM]: Dumping specific signals to novas.fsdb}
     dump -file novas.fsdb -type FSDB
     dump -add tb.dut.clk_i -fid FSDB0
     dump -add tb.dut.db_level_o -fid FSDB0
@@ -56,8 +57,14 @@ proc dump_custom {} {
     quit
 }
 
+proc dump_none {} {
+    # Run simulation without waveform dumping
+    puts {[TCL-CUSTOM]: Running without waveform dumping}
+    run
+    quit
+}
+
 #==============================================================================
 #========================== RUN FUNCTIONS HERE ================================
 #==============================================================================
 
-dump_all
---
+++ verification/uvm/scripts/tcl/none.tcl
@@ -0,0 +1,26 @@
+##==============================================================================
+## [Filename]       none.tcl
+## [Project]        -
+## [Author]         Ciro Bermudez - cirofabian.bermudezmarquez@ba.infn.it
+## [Language]       Tcl (Tool Command Language)
+## [Created]        -
+## [Modified]       -
+## [Description]    Tcl file to run and dump signals to FSDB
+## [Notes]          This file is passed to the ./simv command
+##                  using the -ucli -do dump.tcl flag
+##
+##                  For more information refer to:
+##                    Unified Command Line Interface (UCLI) User Guide W-2024.09
+##                    dump - pag. 106
+## [Status]         stable
+## [Revisions]      -
+##==============================================================================
+
+# Configure directories
+set tcl_dir [file dirname [info script]]
+
+# Source dependencies
+source [file join $tcl_dir dump_lib.tcl]
+
+# Call function
+dump_none
